a725ffe076fb869e2c8aa643b5b4581fafaff2b9
mr-Lj-hub <lakshyajaiswal001@gmail.com>
2023-10-25 15:41:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Body from './components/body/Body';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./Resume.module.css";
+
+function Resume(props) {
+  return <div className={styles.container}></div>;
+  
+}
+
+export default Resume
